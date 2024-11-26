7818cbb55e9bb8f83f25db146fd2853bc8479175
dh24groupA <dh24groupa@gmail.com>
2024-11-26 23:58:28 +0900

    Update ContentView.swift

--- DrugCertificationForVisionOS/ContentView.swift
+++ DrugCertificationForVisionOS/ContentView.swift
@@ -8,13 +8,25 @@
 import SwiftUI
 import RealityKit
 import AVFoundation
+import Vision
 
 struct ContentView: View {
-    @State private var isAuthenticated = true // デフォルトはfalse、テスト用にtrue
-    @State private var isRecording = false
     @State private var showDatePicker = false
     @State private var recordDate = Calendar.current.date(byAdding: .day, value: -1, to: Date()) ?? Date()
     @State var isShowAlert = false
+    @State var pqrData = ""
+    @State var mqrData = ""
+    
+    //本来はQRコードの値を読み取って出したい奴ら
+    @State var pqr1Data = "20000"
+    @State var pqr2Data = "20001"
+    @State var mqr1Data = "mnbvc"
+    @State var mqr2Data = "lkjhg"
+    @State var mqr3Data = "poiuy"
+    @State var mqr4Data = "zxcvb"
+    @State var mqr5Data = "qwert"
+    @State var mqr6Data = "asdfg"
+    
     @State private var patientName = ""
     @State private var patientID = ""
     @State private var medicineName = ""
@@ -28,7 +40,7 @@ struct ContentView: View {
         case finished
     }
     
-    @State private var setting = Setting.title
+    @State private var setting = Setting.patientAuth
     
     var body: some View {
         ZStack {
@@ -39,6 +51,7 @@ struct ContentView: View {
                         .font(.largeTitle)
                         .padding()
                     
+                    
                     Button("認証開始") {
                         setting = .drugAuth
                     }
@@ -47,154 +60,264 @@ struct ContentView: View {
                     .foregroundColor(.white)
                     .cornerRadius(10)
                 }
-            
+                
             case .drugAuth:
-                VStack {
-                    HStack {
-                        Button(action: {
-                            setting = .title
-                        }) {
-                            Text("戻る")
+                ZStack{
+                    VStack {
+                        HStack {
+                            Button(action: {
+                                setting = .title
+                            }) {
+                                Text("戻る")
+                            }
+                            .padding(.leading)
+                            Spacer()
+                            
+                            Text("薬剤選択画面")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .frame(maxWidth: .infinity, alignment: .top)
+                                .padding(.trailing,100)//微調整
+                            
                         }
-                        .padding(.leading)
-                        
+                        .padding()
                         Spacer()
                         
-                        Button(action: {
-                            setting = .patientAuth
-                        }) {
-                            HStack {
-                                Text("次へ")
+                        VStack{
+                            HStack{
+                                VStack{
+                                    UIKitImageView(imageName: "MQR1")
+                                        .frame(width: 75, height: 75) // 表示サイズを指定
+                                        .padding(80)
+                                    
+                                    Button(action: {
+                                        mqrData = mqr1Data
+                                        setting = .patientAuth
+                                    }) {
+                                        Text("このコードを選ぶ")
+                                    }
+                                }
+                                .padding(.trailing,100)
+                                
+                                VStack{
+                                    UIKitImageView(imageName: "MQR2")
+                                        .frame(width: 75, height: 75) // 表示サイズを指定
+                                        .padding(80)
+                                    
+                                    Button(action: {
+                                        mqrData = mqr2Data
+                                        setting = .patientAuth
+                                    }) {
+                                        Text("このコードを選ぶ")
+                                    }
+                                }
+                                .padding(.trailing,100)
+                                
+                                VStack{
+                                    UIKitImageView(imageName: "MQR3")
+                                        .frame(width: 75, height: 75) // 表示サイズを指定
+                                        .padding(80)
+                                    
+                                    Button(action: {
+                                        mqrData = mqr3Data
+                                        setting = .patientAuth
+                                    }) {
+                                        Text("このコードを選ぶ")
+                                    }
+                                }
                             }
+                            
+                            HStack{
+                                VStack{
+                                    UIKitImageView(imageName: "MQR4")
+                                        .frame(width: 75, height: 75) // 表示サイズを指定
+                                        .padding(80)
+                                    
+                                    Button(action: {
+                                        mqrData = mqr4Data
+                                        setting = .patientAuth
+                                    }) {
+                                        Text("このコードを選ぶ")
+                                    }
+                                }
+                                .padding(.trailing,100)
+                                
+                                VStack{
+                                    UIKitImageView(imageName: "MQR5")
+                                        .frame(width: 75, height: 75) // 表示サイズを指定
+                                        .padding(80)
+                                    
+                                    Button(action: {
+                                        mqrData = mqr5Data
+                                        setting = .patientAuth
+                                    }) {
+                                        Text("このコードを選ぶ")
+                                    }
+                                }
+                                .padding(.trailing,100)
+                                
+                                VStack{
+                                    UIKitImageView(imageName: "MQR6")
+                                        .frame(width: 75, height: 75) // 表示サイズを指定
+                                        .padding(80)
+                                    
+                                    Button(action: {
+                                        mqrData = mqr6Data
+                                        setting = .patientAuth
+                                    }) {
+                                        Text("このコードを選ぶ")
+                                    }
+                                }
+                            }
+                            .padding(.bottom)
                         }
                     }
-                    .padding(.top)
-                    
-                    Text("ここで薬剤のコードを読み取る")
-                        .font(.largeTitle)
-                        .bold()
-                        .padding(.top)
-                    
-                    // 仮の画像表示
-                    Image("medicine_bin")
-                        .resizable()
-                        .frame(width: 150, height: 300)
-                        .padding()
                 }
                 .onAppear {
                     medicineName = "お薬飲めたね"
-                    medicineID = "093"
+                    medicineID = mqrData
+                    
                 }
-            
+                
             case .patientAuth:
-                VStack {
-                    HStack {
-                        Button(action: {
-                            setting = .drugAuth
-                        }) {
-                            Text("戻る")
+                ZStack{
+                    VStack {
+                        HStack {
+                            Button(action: {
+                                setting = .title
+                            }) {
+                                Text("戻る")
+                            }
+                            .padding(.leading)
+                            Spacer()
+                            
+                            Text("患者選択画面")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .frame(maxWidth: .infinity, alignment: .top)
+                                .padding(.trailing,100)//微調整
+                            
                         }
-                        .padding(.leading)
-                        
+                        .padding()
                         Spacer()
                         
-                        Button(action: {
-                            setting = .scanned
-                        }) {
-                            HStack {
-                                Text("次へ")
+                        HStack{
+                            VStack{
+                                UIKitImageView(imageName: "PQR1")
+                                    .frame(width: 75, height: 75) // 表示サイズを指定
+                                    .padding(80)
+                                
+                                Button(action: {
+                                    pqrData = pqr1Data
+                                    setting = .scanned
+                                }) {
+                                    Text("このコードを選ぶ")
+                                }
+                            }
+                            .padding(.trailing,200)
+                            
+                            VStack{
+                                UIKitImageView(imageName: "PQR2")
+                                    .frame(width: 75, height: 75) // 表示サイズを指定
+                                    .padding(80)
+                                
+                                Button(action: {
+                                    pqrData = pqr1Data
+                                    setting = .scanned
+                                }) {
+                                    Text("このコードを選ぶ")
+                                }
                             }
                         }
+                        .padding(.bottom,200)
                     }
-                    .padding(.top)
-                    
-                    Text("ここで患者のコードを読み取る")
-                        .font(.largeTitle)
-                        .bold()
-                        .padding(.top)
-                    
-                    // 仮の画像表示
-                    Image("keirou_couple2")
-                        .resizable()
-                        .frame(width: 300, height: 300)
-                        .padding()
                 }
                 .onAppear {
                     patientName = "山田 太郎"
                     patientID = "3849872"
                 }
-
-            
+                
+                
             case .scanned:
-                VStack {
-                    HStack {
-                        Button(action: {
-                            setting = .patientAuth
-                        }) {
-                            Text("戻る")
-                        }
-                        .padding(.leading)
-                        
-                        Spacer()
-                        
-                        Text("薬剤投与前最終確認")
-                            .font(.largeTitle)
-                            .bold()
-                            .padding(.bottom)
-                            .padding(.horizontal)
-                        
-                        Spacer()
-                        
-                        Button(role: .destructive) {
-                            isShowAlert = true
-                        } label: {
-                            Label("投与完了する", systemImage: "pencil.and.outline")
-                        }
-                        .alert("本当に完了しますか？", isPresented: $isShowAlert) {
-                            Button("No") {}
-                            Button("Yes") {
-                                setting = .finished
+                ZStack{
+                    VStack {
+                        HStack {
+                            Button(action: {
+                                setting = .patientAuth
+                            }) {
+                                Text("戻る")
                             }
-                        } message: {
-                            Text("本当に？？")
+                            .padding()
+                            Spacer()
+                            
+                            Text("薬剤投与前最終確認")
+                                .font(.largeTitle)
+                                .bold()
+                                .padding(.bottom)
+                                .padding(.horizontal)
+                            
+                            Spacer()
+                            
+                            Button(role: .destructive) {
+                                isShowAlert = true
+                            } label: {
+                                Label("投与完了する", systemImage: "pencil.and.outline")
+                            }
+                            .alert("本当に完了しますか？", isPresented: $isShowAlert) {
+                                Button("No") {}
+                                Button("Yes") {
+                                    setting = .finished
+                                }
+                            } message: {
+                                Text("完了すると看護記録に投与履歴が送信されます")
+                            }
                         }
-                    }
-                    .padding(.top)
-                    
-                    HStack {
-                        VStack(alignment: .leading, spacing: 10) {
-                            HStack {
-                                Text("記録日:")
-                                Text("\(formattedDate(recordDate))")
-                                    .onTapGesture {
-                                        withAnimation {
-                                            showDatePicker.toggle()
+                        .padding()
+                        Spacer()
+                        
+                        HStack {
+                            VStack(alignment: .leading, spacing: 10) {
+                                HStack {
+                                    Text("記録日:")
+                                    Text("\(formattedDate(recordDate))")
+                                        .onTapGesture {
+                                            withAnimation {
+                                                showDatePicker.toggle()
+                                            }
                                         }
-                                    }
+                                }
+                                Text("患者名: \(patientName.isEmpty ? "未入力" : patientName)")
+                                Text("患者ID: \(patientID.isEmpty ? "未入力" : patientID)")
                             }
-                            Text("患者名: \(patientName.isEmpty ? "未入力" : patientName)")
-                            Text("患者ID: \(patientID.isEmpty ? "未入力" : patientID)")
+                            .padding()
+                            
+                            VStack(alignment: .leading, spacing: 10) {
+                                Text("薬品名: \(medicineName.isEmpty ? "未入力" : medicineName)")
+                                Text("投与方法: 点滴")
+                                Text("投与用量: 800ml")
+                            }
                         }
                         .padding()
                         
-                        VStack(alignment: .leading, spacing: 10) {
-                            Text("薬品名: \(medicineName.isEmpty ? "未入力" : medicineName)")
-                            Text("投与方法: 点滴")
-                            Text("投与用量: 800ml")
+                        if showDatePicker {
+                            DatePicker("記録日", selection: $recordDate, in: Date() - 1...Date(), displayedComponents: .date)
+                                .datePickerStyle(WheelDatePickerStyle())
+                                .labelsHidden()
+                                .environment(\.locale, Locale(identifier: "ja_JP"))
+                                .frame(height: 150)
+                                .padding()
                         }
                     }
-                    .padding()
-                    
-                    if showDatePicker {
-                        DatePicker("記録日", selection: $recordDate, in: Date() - 1...Date(), displayedComponents: .date)
-                            .datePickerStyle(WheelDatePickerStyle())
-                            .labelsHidden()
-                            .environment(\.locale, Locale(identifier: "ja_JP"))
-                            .frame(height: 150)
-                            .padding()
-                    }
                 }
-  
+                .onAppear {
+                    medicineName = getJsonData(searchKey: "id", searchValue: mqrData, returnKey: "name", fileName: "medicines", type: medicine.self)
+                    
+                    patientName = getJsonData(searchKey: "id", searchValue: pqrData, returnKey: "name", fileName: "patients", type: patient.self)
+                    
+                    
+                }
+                
+                
             case .finished:
                 VStack {
                     Text("投与を記録しました")
@@ -202,6 +325,7 @@ struct ContentView: View {
                     
                     Button(action: {
                         setting = .title
+                        
                     }) {
                         Text("最初から記録する")
                     }
@@ -216,7 +340,6 @@ func formattedDate(_ date: Date) -> String {
     formatter.dateFormat = "yy/MM/dd"
     return formatter.string(from: date)
 }
-
 
 #Preview(windowStyle: .automatic) {
     ContentView()
